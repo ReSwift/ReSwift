e30fef53c9b319e40de5bdac03a187c42cd2fa0f
Sendy Halim <sendyhalim93@gmail.com>
2016-01-20 22:23:39 +0700

    Update tests, use optional chaining

--- ReSwiftTests/StoreMiddlewareTests.swift
+++ ReSwiftTests/StoreMiddlewareTests.swift
@@ -44,7 +44,7 @@ let dispatchingMiddleware: Middleware = { dispatch, getState in
         return { action in
 
             if var action = action as? SetValueAction {
-                dispatch(SetValueStringAction("\(action.value)"))
+                dispatch?(SetValueStringAction("\(action.value)"))
 
                 return "Converted Action Successfully"
             }
@@ -64,7 +64,7 @@ let stateAccessingMiddleware: Middleware = { dispatch, getState in
             // avoid endless recursion by checking if we've dispatched exactly this action
             if appState?.testValue == "OK" && stringAction?.value != "Not OK" {
                 // dispatch a new action
-                dispatch(SetValueStringAction("Not OK"))
+                dispatch?(SetValueStringAction("Not OK"))
 
                 // and swallow the current one
                 return next(StandardAction("No-Op-Action"))
