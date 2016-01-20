b73d549e3e0c095b1eee5c286a5cf7e9aab6fc33
Sendy Halim <sendyhalim93@gmail.com>
2016-01-20 22:21:54 +0700

    Set return type of `GetState` to be an `Optional<StateType>`

--- ReSwift/CoreTypes/Middleware.swift
+++ ReSwift/CoreTypes/Middleware.swift
@@ -9,5 +9,5 @@
 import Foundation
 
 public typealias DispatchFunction =  (Action) -> Any
-public typealias GetState = () -> StateType
+public typealias GetState = () -> StateType?
 public typealias Middleware = (DispatchFunction, GetState) -> DispatchFunction -> DispatchFunction
