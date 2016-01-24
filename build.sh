e39556a462f823d4b3e23fe77d95601abe7b47df
Karl Bowden <karl@karlbowden.com>
2016-01-24 11:28:36 +1100

    [Build script] Move branch settings from jazzy config to build script

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@
 JAZZY_REPO=https://github.com/agentk/jazzy/
 JAZZY_BRANCH=integrated-markdown
 
-DOCS_REPO=https://github.com/ReSwift/ReSwift.git
+DOCS_REPO=https://github.com/ReSwift/ReSwift
 DOCS_BRANCH=master
+DOCS_URL=http://ReSwift.github.io/ReSwift
 DOCS_NAME=ReSwift
 
 # LATEST_VERSION_TAG=`git ls-remote --tags $DOCS_REPO | awk -F/ '{ print $3 }' | sort -r | grep -E "^\d+.+\d$" | head -n 1`
@@ -15,7 +16,7 @@ DOCS_NAME=ReSwift
 
 source .build_helpers
 
-update_jazzy "$JAZZY_REPO" "$JAZZY_BRANCH"
+#update_jazzy "$JAZZY_REPO" "$JAZZY_BRANCH"
 
-generate_docs_for_branch "$DOCS_REPO" "$DOCS_BRANCH" "$DOCS_NAME"
+generate_docs_for_branch "$DOCS_REPO" "$DOCS_BRANCH" "$DOCS_URL" "$DOCS_NAME"
 cp $DOCS_NAME/Readme/img/* $DOCS_BRANCH/img/
