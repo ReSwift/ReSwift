b775e230c45a64401b8d3afd55b7dd1d16c9966b
Benji Encz <Benjamin.Encz@gmail.com>
2016-01-23 22:46:12 -0800

    [Subscriber] Add Test Case for Complex Selector

--- ReSwiftTests/StoreSubscriberSpec.swift
+++ ReSwiftTests/StoreSubscriberSpec.swift
@@ -17,16 +17,9 @@ class FilteredStoreSpec: QuickSpec {
 
         describe("#subscribe") {
 
-            var store: Store<TestAppState>!
-            var reducer: TestReducer!
-
-            beforeEach {
-                reducer = TestReducer()
-                store = Store(reducer: reducer, state: TestAppState())
-            }
-
-            it("dispatches initial value upon subscription") {
-                store = Store(reducer: reducer, state: TestAppState())
+            it("allows to pass a state selector closure") {
+                let reducer = TestReducer()
+                let store = Store(reducer: reducer, state: TestAppState())
                 let subscriber = TestFilteredSubscriber()
 
                 store.subscribe(subscriber) {
@@ -36,6 +29,27 @@ class FilteredStoreSpec: QuickSpec {
                 store.dispatch(SetValueAction(3))
 
                 expect(subscriber.receivedValue).to(equal(3))
+            }
+
+            it("supports complex state selector closures") {
+                let reducer = TestComplexAppStateReducer()
+                let store = Store(reducer: reducer, state: TestComplexAppState())
+                let subscriber = TestSelectiveSubscriber()
+
+                store.subscribe(subscriber) {
+                    (
+                        $0.testValue,
+                        $0.otherState?.name
+                    )
+                }
+
+                store.dispatch(SetValueAction(5))
+                store.dispatch(SetOtherStateAction(
+                    otherState: OtherState(name: "TestName", age: 99)
+                ))
+
+                expect(subscriber.receivedValue.0).to(equal(5))
+                expect(subscriber.receivedValue.1).to(equal("TestName"))
             }
         }
 
@@ -50,4 +64,48 @@ class TestFilteredSubscriber: StoreSubscriber {
         receivedValue = state
     }
 
+}
+
+/**
+ Example of how you can select a substate. The return value from
+ `selectSubstate` and the argument for `newState` need to match up.
+ */
+class TestSelectiveSubscriber: StoreSubscriber {
+    var receivedValue: (Int?, String?)
+
+    func newState(state: (Int?, String?)) {
+        receivedValue = state
+    }
+}
+
+struct TestComplexAppState: StateType {
+    var testValue: Int?
+    var otherState: OtherState?
+}
+
+struct OtherState {
+    var name: String?
+    var age: Int?
+}
+
+struct TestComplexAppStateReducer: Reducer {
+    func handleAction(action: Action, state: TestComplexAppState?) -> TestComplexAppState {
+        var state = state ?? TestComplexAppState()
+
+        switch action {
+        case let action as SetValueAction:
+            state.testValue = action.value
+            return state
+        case let action as SetOtherStateAction:
+            state.otherState = action.otherState
+        default:
+            break
+        }
+
+        return state
+    }
+}
+
+struct SetOtherStateAction: Action {
+    var otherState: OtherState
 }
