2bb8d8b9c6885de6bc1bb52bfbd9066696121850
Karl Bowden <karl@karlbowden.com>
2016-02-26 00:08:26 +1100

    [master] Regenerate documentation

--- build.sh
+++ build.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-# ----- Configuration
-
-JAZZY_REPO=https://github.com/agentk/jazzy/
-JAZZY_BRANCH=integrated-markdown
-
-DOCS_REPO=https://github.com/ReSwift/ReSwift
-DOCS_BRANCH=master
-DOCS_URL=http://ReSwift.github.io/ReSwift
-DOCS_NAME=ReSwift
-
-# LATEST_VERSION_TAG=`git ls-remote --tags $DOCS_REPO | awk -F/ '{ print $3 }' | sort -r | grep -E "^\d+.+\d$" | head -n 1`
-
-# ----- Setup and generate docs
-
 source .build_helpers
 
-#update_jazzy "$JAZZY_REPO" "$JAZZY_BRANCH"
+update_jazzy \
+  https://github.com/agentk/jazzy/ \
+  integrated-markdown
 
-generate_docs_for_branch "$DOCS_REPO" "$DOCS_BRANCH" "$DOCS_URL" "$DOCS_NAME"
-cp $DOCS_NAME/Readme/img/* $DOCS_BRANCH/img/
+generate_docs__org_name_branch \
+    ReSwift ReSwift master
+
