31fe6386c78b632f2a08e318c77e798f13d391fa
Malcolm Jarvis <malcolm@getflow.com>
2017-01-20 14:23:10 -0800

    Add DispatchingStoreType protocol
    Defines the interface of a dispatching, stateless Store. Useful for dependency injection when your type only cares about dispatching raw actions, and doesn’t need to know about the store’s state.
    
    Fixes #174

---
+++ ReSwift/CoreTypes/DispatchingStoreType.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+/**
+ Defines the interface of a dispatching, stateless Store in ReSwift. `StoreType` is 
+ the default usage of this interface. Can be used for store variables where you don't
+ care about the state, but want to be able to dispatch actions.
+ */
+public protocol DispatchingStoreType {
+
+    /**
+     Dispatches an action. This is the simplest way to modify the stores state.
+
+     Example of dispatching an action:
+
+     ```
+     store.dispatch( CounterAction.IncreaseCounter )
+     ```
+
+     - parameter action: The action that is being dispatched to the store
+     - returns: By default returns the dispatched action, but middlewares can change the
+     return type, e.g. to return promises
+     */
+    func dispatch(_ action: Action)
+}
