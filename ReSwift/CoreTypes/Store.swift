cbd188a4a45a51a8fb682f48cb70ea74813422d9
Benji Encz <Benjamin.Encz@gmail.com>
2017-03-23 08:27:38 -0700

    [Cleanup] Remove Superfluous DiscardableResult

--- ReSwift/CoreTypes/Store.swift
+++ ReSwift/CoreTypes/Store.swift
@@ -83,7 +83,6 @@ open class Store<State: StateType>: StoreType {
             _ = subscribe(subscriber, transform: nil)
     }
 
-    @discardableResult
     open func subscribe<SelectedState, S: StoreSubscriber>(
         _ subscriber: S, transform: ((Subscription<State>) -> Subscription<SelectedState>)?
     ) where S.StoreSubscriberStateType == SelectedState
