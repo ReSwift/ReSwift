7795e65e8af23d7cb51acb4fb8afa90c8f7b4907
Malcolm Jarvis <malcolm@getflow.com>
2017-05-03 09:56:37 -0700

    Add Subscription `skip(when:)` and `only(when:)`
    These subscription filtering methods are provided as convenience methods for `skipRepeats`.
    
    1. `skip(when:)` is a direct forward to `skipRepeats` and is simply provided as a more swift-like naming option.
    2. `only(when:)` is the inverse to `skipRepeats`, and is provided for specifying when the user wants the subscription to notify, as opposed to when to skip notifying.
    
    Both of these functions are technically "sugar", however I feel like they add value to the library by making the available use cases of `skipRepeats` more obvious to users.
    
    For example, a user could chain calls to these, such as `$0.select({ $0.child }).skip(when: { $0.identifier == $1.identifier }).only(when: { $1.name.lowercased.hasPrefix("a") })` which would notify the subscriber when the child changes, and the new child's `name` starts with "a"

--- ReSwift/CoreTypes/Subscription.swift
+++ ReSwift/CoreTypes/Subscription.swift
@@ -143,3 +143,31 @@ extension Subscription where State: Equatable {
         return self.skipRepeats(==)
     }
 }
+
+/// Subscription skipping convenience methods
+extension Subscription {
+
+    /// Provides a subscription that skips certain state updates of the original subscription.
+    ///
+    /// This is identical to `skipRepeats` and is provided simply for convenience.
+    /// - parameter when: A closure that determines whether a given state update is a repeat and
+    /// thus should be skipped and not forwarded to subscribers.
+    /// - parameter oldState: The store's old state, before the action is reduced.
+    /// - parameter newState: The store's new state, after the action has been reduced.
+    public func skip(when: @escaping (_ oldState: State, _ newState: State) -> Bool) -> Subscription<State> {
+        return self.skipRepeats(when)
+    }
+
+    /// Provides a subscription that only updates for certain state changes.
+    ///
+    /// This is effectively the inverse of `skip(when:)` / `skipRepeats(:)`
+    /// - parameter when: A closure that determines whether a given state update should notify
+    /// - parameter oldState: The store's old state, before the action is reduced.
+    /// - parameter newState: The store's new state, after the action has been reduced.
+    /// the subscriber.
+    public func only(when: @escaping (_ oldState: State, _ newState: State) -> Bool) -> Subscription<State> {
+        return self.skipRepeats { oldState, newState in
+            return !when(oldState, newState)
+        }
+    }
+}
