cf6b5efbd032c82492bddb0a559961a7cbd2ab71
Victor Carvalho Tavernari <victortavernari@gmail.com>
2018-07-04 17:52:04 -0300

    fix Colon Violation

--- ReSwift/Utils/MiddlewaresCollection.swift
+++ ReSwift/Utils/MiddlewaresCollection.swift
@@ -1,24 +1,24 @@
 import Foundation
 import ReSwift
 
-class MiddlewareExecutor<T:StateType>{
-    func execute(action:Action, state:T?, nextDispatcher:@escaping DispatchFunction) -> Action?{
+class MiddlewareExecutor<T: StateType> {
+    func execute(action: Action, state: T?, nextDispatcher: @escaping DispatchFunction) -> Action?{
         return nil
     }
 }
 
-class MiddlewaresCollection<T:StateType>{
-    private var _middlewares:[Middleware<T>]
+class MiddlewaresCollection<T: StateType> {
+    private var _middlewares: [Middleware<T>]
     init(){
         self._middlewares = []
     }
     
-    func concact(withCollection:MiddlewaresCollection)->MiddlewaresCollection{
+    func concact(withCollection: MiddlewaresCollection) -> MiddlewaresCollection {
         self._middlewares = _middlewares + withCollection.middlewares
         return self
     }
     
-    func add(_ middlewareItens:MiddlewareExecutor<T>...)->MiddlewaresCollection{
+    func add(_ middlewareItens: MiddlewareExecutor<T>...) -> MiddlewaresCollection {
         for item in middlewareItens {
             self._middlewares.append ({ (_, state) -> (@escaping DispatchFunction) -> DispatchFunction in
                 return { next in
@@ -34,7 +34,7 @@ class MiddlewaresCollection<T:StateType>{
         return self
     }
     
-    public var middlewares:[Middleware<T>]{
+    public var middlewares: [Middleware<T>] {
         return self._middlewares
     }
 }
