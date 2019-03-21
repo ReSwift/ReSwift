ReSwift/ReSwift

commit bdb648f1b7817dfe31c12edac0fa2c475643bf66

remove select-variant that used skipRepeats

ReSwiftTests/StoreSubscriberTests.swift
@@ -120,10 +120,7 @@ class StoreSubscriberTests: XCTestCase {
         XCTAssertEqual(subscriber.newStateCallCount, 1)
     }
 
-    /**
-     it skips repeated state values by default when the selected substate is `Equatable`.
-     */
-    func testSkipsStateUpdatesForEquatableSubstatesByDefault() {
+    func testPassesOnDuplicateSubstateUpdatesByDefault() {
         let reducer = TestValueStringReducer()
         let state = TestStringAppState()
         let store = Store(reducer: reducer.handleAction, state: state)
@@ -138,7 +135,7 @@ class StoreSubscriberTests: XCTestCase {
         store.dispatch(SetValueStringAction("Initial"))
 
         XCTAssertEqual(subscriber.receivedValue, "Initial")
-        XCTAssertEqual(subscriber.newStateCallCount, 1)
+        XCTAssertEqual(subscriber.newStateCallCount, 2)
     }
 
     func testSkipsStateUpdatesForEquatableStateByDefault() {

ReSwift/CoreTypes/Subscription.swift
@@ -80,16 +80,6 @@ public class Subscription<State> {
         return self._select(selector)
     }
 
-    /// Provides a subscription that selects a substate of the state of the original subscription.
-    /// If the selected substate is `Equatable` repeated state updates will be skipped.
-    /// - parameter selector: A closure that maps a state to a selected substate
-    public func select<Substate: Equatable>(
-        _ selector: @escaping (State) -> Substate
-        ) -> Subscription<Substate>
-    {
-        return self._select(selector).skipRepeats()
-    }
-
     /// Provides a subscription that skips certain state updates of the original subscription.
     /// - parameter isRepeat: A closure that determines whether a given state update is a repeat and
     /// thus should be skipped and not forwarded to subscribers.

commit efc4c12772f72f8a46026828b3991d41c46537c2

Fix regression with `automaticallySkipsRepeats` when selecting Equatable state in Non-Equatable root state
If one's Store's base state did not conform to `Equatable`, the appropriate sub-state selection which skips repeats would not be selected.

This change ensures that `automaticallySkipsRepeats` works as expected in this case.

Fixes #394 

Regression was caused in https://github.com/ReSwift/ReSwift/commit/bdb648f1b7817dfe31c12edac0fa2c475643bf66 which was merged in #262

ReSwift/CoreTypes/Store.swift
@@ -201,16 +201,7 @@ open class Store<State: StateType>: StoreType {
 
 // MARK: Skip Repeats for Equatable States
 
-extension Store where State: Equatable {
-    open func subscribe<S: StoreSubscriber>(_ subscriber: S)
-        where S.StoreSubscriberStateType == State {
-            guard subscriptionsAutomaticallySkipRepeats else {
-                _ = subscribe(subscriber, transform: nil)
-                return
-            }
-            _ = subscribe(subscriber, transform: { $0.skipRepeats() })
-    }
-
+extension Store {
     open func subscribe<SelectedState: Equatable, S: StoreSubscriber>(
         _ subscriber: S, transform: ((Subscription<State>) -> Subscription<SelectedState>)?
         ) where S.StoreSubscriberStateType == SelectedState
@@ -223,5 +214,16 @@ extension Store where State: Equatable {
         }
         _subscribe(subscriber, originalSubscription: originalSubscription,
                    transformedSubscription: transformedSubscription)
+    }
+}
+
+extension Store where State: Equatable {
+    open func subscribe<S: StoreSubscriber>(_ subscriber: S)
+        where S.StoreSubscriberStateType == State {
+            guard subscriptionsAutomaticallySkipRepeats else {
+                _ = subscribe(subscriber, transform: nil)
+                return
+            }
+            _ = subscribe(subscriber, transform: { $0.skipRepeats() })
     }
 }

ReSwiftTests/StoreSubscriberTests.swift
@@ -180,6 +180,24 @@ class StoreSubscriberTests: XCTestCase {
         XCTAssertEqual(subscriber.newStateCallCount, 1)
     }
 
+    func testSkipsStateUpdatesForEquatableSubStateByDefault() {
+        let reducer = TestNonEquatableReducer()
+        let state = TestNonEquatable()
+        let store = Store(reducer: reducer.handleAction, state: state)
+        let subscriber = TestFilteredSubscriber<String>()
+
+        store.subscribe(subscriber) {
+            $0.select { $0.testValue.testValue }
+        }
+
+        XCTAssertEqual(subscriber.receivedValue, "Initial")
+
+        store.dispatch(SetValueStringAction("Initial"))
+
+        XCTAssertEqual(subscriber.receivedValue, "Initial")
+        XCTAssertEqual(subscriber.newStateCallCount, 1)
+    }
+
     func testPassesOnDuplicateStateUpdatesInCustomizedStore() {
         let reducer = TestValueStringReducer()
         let state = TestStringAppState()

ReSwiftTests/StoreSubscriptionTests.swift
@@ -305,7 +305,7 @@ extension StoreSubscriptionTests {
 
         autoreleasepool {
 
-            store = TestStore(reducer: reducer.handleAction, state: TestAppState())
+            store = TestStore(reducer: reducer.handleAction, state: TestAppState(), automaticallySkipsRepeats: false)
             let subscriber = TestStoreSubscriber<Int?>()
 
             // Preconditions