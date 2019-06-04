908b6304411b3ecc9374965869f7ba63a8f691af
hoemoon <hoemoon.song@gmail.com>
2019-06-04 19:01:31 +0900

    Fix typo

--- Package@swift-5.0.swift
+++ Package@swift-5.0.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.0
 import PackageDescription
 
-let pkg = Package(name "ReSwift")
+let pkg = Package(name: "ReSwift")
 pkg.platforms = [
     .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
 ]
