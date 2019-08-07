d39aaf108c987fe7659a5fafe39019bd30c8c3fb
Christian Tietze <christian.tietze@gmail.com>
2019-08-07 18:05:11 +0200

    change organization name to "ReSwift Community" (#421)

--- ReSwift/CoreTypes/Reducer.swift
+++ ReSwift/CoreTypes/Reducer.swift
@@ -3,7 +3,7 @@
 //  ReSwift
 //
 //  Created by Benjamin Encz on 12/14/15.
-//  Copyright © 2015 Benjamin Encz. All rights reserved.
+//  Copyright © 2015 ReSwift Community. All rights reserved.
 //
 
 public typealias Reducer<ReducerStateType> =
